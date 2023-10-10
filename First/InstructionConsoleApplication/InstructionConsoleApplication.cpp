477f5ad9d8d9250065f7d357ee1ab5f50b4c8d0c
DeYwid123 <dpustula1314@gmail.com>
2023-10-10 16:34:17 +0200

    wszystkie zadania

--- First/InstructionConsoleApplication/InstructionConsoleApplication.cpp
+++ First/InstructionConsoleApplication/InstructionConsoleApplication.cpp
@@ -17,8 +17,11 @@ Operatory porównania:
 3. 
 4. 
 5. 
-6. Program sprawdzający czy podana data jest poprawna (np. sprawdzając, czy dzień jest z zakresu od 1 do 31, miesiąc od 1 do 12 itd.)
+6. 
 7. Program wyświetlający odpowiedni komunikat w zależności od podanej temperatury (np. "ciepło" dla temperatury powyżej 20 stopni Celsjusza, "chłodno" dla temperatury poniżej 10 stopni Celsjusza itd.)
+8. 
+9. 
+10. 
 */
 
 
@@ -158,13 +161,102 @@ void task7()
     std::cin >> month;
     std::cout << "Podaj dzien:\n";
     std::cin >> day;
+    
+    bool date = year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
 
-    if (year > 0 || year < 2023 || month >= 1 || month <= 12 || day >= 1 || day <= 31 )
+    if (day < 1
+        || day >  31
+
+        || month < 1
+        || month < 12
+
+        || (day == 31 && (month == 4 || month == 6 || month == 9 || month ==11))
+
+        || (month == 2 && day > 29)
+
+        || (month == 2 && day > 29 && date)
+        )
     {
-        std::cout << "poprawna data";
+        std::cout << "data jest nie poprawana\n";
+    }
+    else
+    {
+        std::cout << "poprawna data\n";
+    }
+}//Program wyświetlający odpowiedni komunikat w zależności od podanej temperatury (np. "ciepło" dla temperatury powyżej 20 stopni Celsjusza, "chłodno" dla temperatury poniżej 10 stopni Celsjusza itd.)
+void task8()
+{
+    double temp;
+    std::cout << "podaj temperaturen\n";
+    std::cin >> temp;
+    if (temp >= 10 && temp < 20)
+    {
+        std::cout << "cieplo";
+    }
+    else if (temp >= 20)
+    {
+        std::cout << "goraco";
+    }
+    else if (temp >= 0 && temp < 10)
+    {
+        std::cout << "chlodno";
+    }
+    else if (temp < 0)
+    {
+        std::cout << "zimno";
     }
 }
+//8. Program sprawdzjący czy podana liczba jest z przediału <1;15)
+void task9()
+{
+    int number;
+    std::cout << "podaj liczbe:\n";
+    std::cin >> number;
+    if (number >= 1 && number < 15)
+    {
+        std::cout << "podana liczba jest z przedzialu <1;15)";
+    }
+    else
+    {
+        std::cout << "podana liczba nie jest z przedzialu <1;15)";
 
+    }
+}
+//9. Program sprawdzający czy osoba jest pełnoletnia.
+void task10()
+{
+    int age;
+    std::cout << "podaj wiek:\n";
+    std::cin >> age;
+    if (age >= 18)
+    {
+        std::cout << "jestes pelnoletni";
+    }
+    else
+    {
+        std::cout << "nie jestes pelnoletni";
+
+    }
+}
+//10. Program, który sprawdzi czy z podanych długości boków można zrobić trójkąt.
+void task11()
+{
+    int side1, side2, base;
+    std::cout << "Podaj pierwsze ramie:\n";
+    std::cin >> side1;
+    std::cout << "Podaj drugie ramie:\n";
+    std::cin >> side2;
+    std::cout << "Podaj podstawe:\n";
+    std::cin >> base;
+    if (base >= 1 && side1 >= 1 && side2 >= 1)
+    {
+        std::cout << "z tych bokow mozna zrobic trojkat";
+    }
+    else
+    {
+        std::cout << "z tych bokow nie mozna zrobic trojkata";
+    }
+}
 int main()
 {
     setlocale(LC_CTYPE, "polish");
@@ -176,4 +268,8 @@ int main()
     //task5();
     //task6();
     //task7();
+    //task8();
+    //task9();
+    //task10();
+    //task11();
 }
