c7c60aeeae4c04900ba2cd3d7090b6bc63481c52
DeYwid123 <dpustula1314@gmail.com>
2023-11-21 16:27:52 +0100

    git

--- First/StringConsoleApplication/StringConsoleApplication.cpp
+++ First/StringConsoleApplication/StringConsoleApplication.cpp
@@ -3,6 +3,24 @@
 
 void task1()
 {
+	char signFromUser;
+	//signFromUser = 'a';
+	std::cout << "Podaj znak\n";
+	std::cin >> signFromUser;
+
+	std::cout << "Podales znak" << signFromUser << "\n";
+	
+	//if (signFromUser >= 97 && signFromUser <= 122)
+	if (signFromUser >= 'a' && signFromUser <= 'z')
+		std::cout << "znak jest mala litera alfabetu\n";
+	else
+		std::cout << "znak nie jest liczba alfabetu\n";
+
+	signFromUser = signFromUser + 2;
+	std::cout << "Podales znak" << signFromUser << "\n";
+
+	signFromUser = 'a' + 2;
+	std::cout << "podales znak" << signFromUser << "\n";
 }
 
 int main()
